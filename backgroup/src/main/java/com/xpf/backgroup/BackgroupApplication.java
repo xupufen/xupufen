b732c347317afebcde2601e271f001a2db4c3147
徐朴芬 <13504388+xu-pufen@user.noreply.gitee.com>
2024-11-08 10:23:44 +0800

    add: 后端springboot3.3.5 前端uni-app(vue3)

---
+++ backgroup/src/main/java/com/xpf/backgroup/BackgroupApplication.java
@@ -0,0 +1,13 @@
+package com.xpf.backgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackgroupApplication.class, args);
+    }
+
+}
---
+++ backgroup/src/test/java/com/xpf/backgroup/BackgroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpf.backgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackgroupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
