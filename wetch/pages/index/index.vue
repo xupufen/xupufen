948e9e09e51dad42e7fe6889efa7b0d0ebd90ae1
徐朴芬 <13504388+xu-pufen@user.noreply.gitee.com>
2024-11-11 17:30:31 +0800

    fix: 视频布局

--- wetch/pages/index/index.vue
+++ wetch/pages/index/index.vue
@@ -1,27 +1,47 @@
 <template>
-	<div class="app">
-		
-	</div>
+  <view class="app">
+    <view class="video">
+      <video autoplay="true" loop="true" muted="true" playsinline src="../../static/login.mp4" controls="false" style="pointer-events: none;" />
+    </view>
+  </view>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
+
 const login = ref({
-	user: '',
-	password: ''
+  user: '',
+  password: ''
 })
 </script>
 
 <style lang="scss">
-	.app{
-		border: 1px solid red;
-		width: 100%;
-		height: 95vh;
-		box-sizing: border-box;
-		display: flex;
-		flex-direction: column;
-		position: absolute;
-		top: 0;
-		left: 0;
-	}
+.app {
+  border: 1px solid red;
+  width: 100%;
+  height: 95vh;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  .video {
+    border: 1px solid red;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    video{
+      border: 1px solid red;
+      width: 100%;
+      height: 100%;
+      z-index: 1;
+      object-fit: cover;
+      transform: scaleY(1.2);
+      transform-origin: center;
+    }
+  }
+}
 </style>
