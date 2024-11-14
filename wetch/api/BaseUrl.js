64218d3535c526b816cef37a9e605f9acd229c7d
徐朴芬 <13504388+xu-pufen@user.noreply.gitee.com>
2024-11-14 23:58:39 +0800

    fix: 新增环境

--- wetch/api/BaseUrl.js
+++ wetch/api/BaseUrl.js
@@ -1,1 +1,3 @@
-export const url ='http://10.135.9.47:8080'
+// export const url ='http://10.135.9.47:8080' //公司环境
+export const url ='http://192.168.2.2:8080' //龙凯环境
+// export const url ='http://192.168.2.2:8080' //本地环境
