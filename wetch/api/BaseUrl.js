634e31edf18944e400fecc9b5b39cf81b78a23ae
徐朴芬 <13504388+xu-pufen@user.noreply.gitee.com>
2024-11-19 17:31:38 +0800

    fix: 线上环境地址

--- wetch/api/BaseUrl.js
+++ wetch/api/BaseUrl.js
@@ -1,3 +1,4 @@
-export const url ='http://10.135.9.47:8080' //公司环境
+// export const url ='http://10.135.9.47:8080' //公司环境
 // export const url ='http://192.168.2.2:8080' //龙凯环境
 // export const url ='http://192.168.2.2:8080' //本地环境
+export const url ='http:/t.xupufen.top:8080' //线上环境
